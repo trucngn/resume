67c1198b2d679b3ac72a75aa39ec3598b234e495
Truc Nguyen <truc.ngn@gmail.com>
2021-01-17 23:37:24 +0100

    dynamic json resume theme package name

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,10 @@ cd ${WORKDIR}
 # retrieve and install jsonresume theme
 wget "$1" -O /theme.tgz
 npm install /theme.tgz
+package_name=$(npm pack /theme.tgz --dry-run 2>&1 | grep "^npm notice name:" | sed 's/npm notice name:\s*\(.*\)/\1/')
 
 # export to HTML
-resume export -t ./node_modules/jsonresume-theme-helloquan resume.html
+resume export -t ./node_modules/$package_name resume.html
 
 # generate PDF
 wkhtmltopdf -s A4 -B 10 -L 10 -R 10 -T 10 --no-background --javascript-delay 3000 resume.html resume.pdf
